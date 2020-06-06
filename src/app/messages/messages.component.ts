f1a83253a21562609a5e59a8a06941be4ffc8ef5
Nelson Foltz <nelsfoltz@gmail.com>
2020-06-06 15:42:17 -0400

    add: completed implementation of messages service

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessagesService } from './messages.service';
 import { Observable } from 'rxjs';
 import { Message } from '../model/message';
 import { tap } from 'rxjs/operators';
@@ -11,9 +12,17 @@ import { tap } from 'rxjs/operators';
 export class MessagesComponent implements OnInit {
   showMessages = false;
 
-  constructor() {}
+  errors$: Observable<string[]>;
 
-  ngOnInit() {}
+  constructor(public messagesService: MessagesService) {
+    console.log('Created messages component');
+  }
+
+  ngOnInit() {
+    this.errors$ = this.messagesService.errors$.pipe(
+      tap(() => (this.showMessages = true))
+    );
+  }
 
   onClose() {
     this.showMessages = false;
