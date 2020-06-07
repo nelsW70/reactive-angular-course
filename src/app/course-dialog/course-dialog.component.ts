2900f63fac9c8c285b9a2adadcbb7d68f724343c
Nelson Foltz <nelsfoltz@gmail.com>
2020-06-07 09:19:22 -0400

    add: store optimistic data modification operations - api design

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,3 +1,4 @@
+import { CoursesStore } from './../services/courses.store';
 import {
   AfterViewInit,
   Component,
@@ -13,15 +14,13 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import * as moment from 'moment';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
-import { CoursesService } from './../services/courses.service';
-import { LoadingService } from './../loading/loading.service';
 import { MessagesService } from './../messages/messages.service';
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css'],
-  providers: [LoadingService, MessagesService],
+  providers: [MessagesService],
 })
 export class CourseDialogComponent implements AfterViewInit {
   form: FormGroup;
@@ -31,8 +30,7 @@ export class CourseDialogComponent implements AfterViewInit {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
-    private coursesService: CoursesService,
-    private loadingService: LoadingService,
+    private coursesStore: CoursesStore,
     private messagesService: MessagesService
   ) {
     this.course = course;
@@ -50,22 +48,8 @@ export class CourseDialogComponent implements AfterViewInit {
   save() {
     const changes = this.form.value;
 
-    const saveCourse$ = this.coursesService
-      .saveCourse(this.course.id, changes)
-      .pipe(
-        catchError((err) => {
-          const message = 'Could not save course';
-          console.log(message, err);
-          this.messagesService.showErrors(message);
-          return throwError(err);
-        })
-      );
-
-    this.loadingService
-      .showLoaderUntilCompleted(saveCourse$)
-      .subscribe((val) => {
-        this.dialogRef.close(val);
-      });
+    this.coursesStore.saveCourse(this.course.id, changes).subscribe();
+    this.dialogRef.close(changes);
   }
 
   close() {
