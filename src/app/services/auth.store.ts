d19bd9ba00b48452f6d631948aadd8ae77f8f79d
Nelson Foltz <nelsfoltz@gmail.com>
2020-06-13 09:15:21 -0400

    add: step 29 - browser refresh support with local storage

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,8 @@ import { Observable, BehaviorSubject } from 'rxjs';
 import { Injectable, Pipe } from '@angular/core';
 import { User } from './../model/user';
 
+const AUTH_DATA = 'auth_data';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,6 +13,7 @@ export class AuthStore {
   private subject = new BehaviorSubject<User>(null);
 
   user$: Observable<User> = this.subject.asObservable();
+
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
 
@@ -18,18 +21,28 @@ export class AuthStore {
     this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
 
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+
+    const user = localStorage.getItem(AUTH_DATA);
+
+    if (user) {
+      this.subject.next(JSON.parse(user));
+    }
   }
 
   login(email: string, password: string): Observable<User> {
     return this.http
       .post<User>('/api/login', { email, password })
       .pipe(
-        tap((user) => this.subject.next(user)),
+        tap((user) => {
+          this.subject.next(user);
+          localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+        }),
         shareReplay()
       );
   }
 
   logout() {
     this.subject.next(null);
+    localStorage.removeItem(AUTH_DATA);
   }
 }
