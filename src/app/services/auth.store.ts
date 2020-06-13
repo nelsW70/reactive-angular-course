78d460436321b6c3d2ec60a7c32c14204cc5fed3
Nelson Foltz <nelsfoltz@gmail.com>
2020-06-13 08:38:08 -0400

    add: step 27 - completed authentication store

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -1,12 +1,35 @@
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, shareReplay, tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Injectable, Pipe } from '@angular/core';
 import { User } from './../model/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthStore {
-  user$: Observable<User>;
+  private subject = new BehaviorSubject<User>(null);
+
+  user$: Observable<User> = this.subject.asObservable();
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
+
+  constructor(private http: HttpClient) {
+    this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
+
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+  }
+
+  login(email: string, password: string): Observable<User> {
+    return this.http
+      .post<User>('/api/login', { email, password })
+      .pipe(
+        tap((user) => this.subject.next(user)),
+        shareReplay()
+      );
+  }
+
+  logout() {
+    this.subject.next(null);
+  }
 }
