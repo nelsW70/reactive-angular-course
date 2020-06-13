17db982891925eab8a38802f78860b9bfe3f9f2f
Nelson Foltz <nelsfoltz@gmail.com>
2020-06-13 10:06:05 -0400

    add: step 31 - implementing master detail pattern for search table

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,8 +1,9 @@
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, filter } from 'rxjs/operators';
 import { Course } from './../model/course';
+import { Lesson } from '../model/lesson';
 
 @Injectable({
   providedIn: 'root',
@@ -21,5 +22,19 @@ export class CoursesService {
     return this.http
       .put(`/api/courses/${courseId}`, changes)
       .pipe(shareReplay());
+  }
+
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>('/api/lessons', {
+        params: {
+          filter: search,
+          pageSize: '100',
+        },
+      })
+      .pipe(
+        map((res) => res['payload']),
+        shareReplay()
+      );
   }
 }
